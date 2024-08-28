fd5f2e70991b9e995bec1911a50bf4d8c4a7b149
IT24101052 <it24101052@my.sliit.lk>
2024-08-28 12:27:44 +0530

    Add IT24101052Lab6Q2A to Q2

---
+++ Q2/IT24101052Lab6Q2A.java
@@ -0,0 +1,13 @@
+public class IT24101052Lab6Q2A
+{
+	public static void main(String[] args)
+	{
+		int count;
+		count=1;
+		while(count<=10)
+		{
+			System.out.print(count+" ");
+			count = count+1;
+		}
+	}
+}
